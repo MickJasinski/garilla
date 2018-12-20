0b77ceb0a41b4bd334da6026e2edc033d82c9dd0
Mick Jasinski <michal.jasinski@t-f-a.co.uk>
2018-12-20 10:12:24 +0000

    Add logo and fb imgs; Add menu;

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import { Link } from 'gatsby'
 import styled from "styled-components"
+
+import logo from '../images/logo.png'
+import fb from '../images/facebook.svg'
 
 // Styled components
 const HeaderWrap = styled.div`
@@ -8,9 +12,47 @@ const HeaderWrap = styled.div`
     background: black;
 `;
 
+const Logo = styled.img`
+    position: absolute;
+    right: 0;
+`;
+
+const Menu = styled.div`
+    height: 100px;
+    width: 500px;
+    float: left;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: 30px
+`;
+
+const MenuList = styled.ul`
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+`;
+
+const MenuLink  = styled(Link)`
+    color: white;
+    text-decoration: none;
+    display: 
+`;
+
+const Fb = styled.img`
+    width: 40px;
+`;
+
 const Header = () => (
     <HeaderWrap>
-
+        <Menu>
+            <Fb src={fb} alt="Facebook Link"/>
+            <MenuList>
+                <li><MenuLink to="/">Home</MenuLink></li>
+                <li><MenuLink to="about">About</MenuLink></li>
+            </MenuList>
+        </Menu>
+        <Logo src={logo} alt="Garilla Logo"/>
     </HeaderWrap>
 );
 
